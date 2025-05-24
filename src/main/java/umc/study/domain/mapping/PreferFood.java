1f01baf4ece77c59f42b98cdccf5c84fa889a5cd
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-24 22:56:35 +0900

    refactor : 객체 불변성 위한 setter 지양 리팩토링

--- src/main/java/umc/study/domain/mapping/PreferFood.java
+++ src/main/java/umc/study/domain/mapping/PreferFood.java
@@ -9,9 +9,10 @@ import umc.study.domain.common.BaseEntity;
 @Entity
 @Getter
 @Builder
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+// @Setter 지양 : 객체지향 설계 핵심 원칙인 불변성과 관련된 문제
+// setter 최소화하고 생성자나 빌더로만 값을 설정하는 의미.
 public class PreferFood extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
