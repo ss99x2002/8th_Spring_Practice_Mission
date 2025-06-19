79866cae2f13cb914cc24c6b2593e3cbbd22f25c
Seohyeon Shin <ss99x200211@gmail.com>
2025-06-19 14:43:08 +0900

    feat : 로그인 구현

--- src/main/java/umc/study/repository/user/UserRepository.java
+++ src/main/java/umc/study/repository/user/UserRepository.java
@@ -3,5 +3,8 @@ package umc.study.repository.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.study.domain.user.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
